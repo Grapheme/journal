428ba32389049793fe1553def0c8ec3e586c6e59
Vladimir Kharseev <vkharseev@gmail.com>
2013-09-19 15:04:49 +0400

    work

--- application/views/html/select-years.php
+++ application/views/html/select-years.php
@@ -2,7 +2,7 @@
 	<ul class="production-years-list">
 	<?php $year = date("Y");?>
 	<?php for($i=$year;$i>=$year-7;$i--):?>
-		<li><a href="<?=site_url(uri_string().'?year='.$i);?>"><?=$i?></a></li>
+		<li><a <?=($this->input->get('year') == $i)?'class="active"':''?> href="<?=site_url(uri_string().'?year='.$i);?>"><?=$i?></a></li>
 	<?php endfor;?>
 	</ul>
 </div>
