e6335617418fdd2c22445d2881271223df4555ee
Vladimir Kharseev <vkharseev@gmail.com>
2013-12-02 12:56:19 +0400

    02.12.2013

---
+++ application/views/xml/issue.php
@@ -0,0 +1,98 @@
+<?xml version='1.0' standalone='no' ?>
+<journal>
+	<operCard>
+		<operator>Articulus_2008</operator>
+		<pid>136378</pid>
+		<date>2013-11-1 0:34:01</date>
+		<cntArticle>1</cntArticle>
+		<cntNode></cntNode>
+		<cs>0</cs>
+	</operCard>
+	<titleid>32527</titleid>
+	<issn>2073-0373</issn>
+	<codeNEB>20730373</codeNEB>
+	<journalInfo lang="RUS">
+		<title>Фазовые переходы, упорядоченные состояния и новые материалы</title>
+	</journalInfo>
+	<issue>
+		<volume></volume>
+		<number>89</number>
+		<altNumber></altNumber>
+		<part></part>
+		<dateUni>201309</dateUni>
+		<issTitle></issTitle>
+		<pages>1-50</pages>
+		<articles>
+			<article>
+				<pages>1-5</pages>
+				<artType>RAR</artType>
+				<authors>
+					<author num="001">
+						<individInfo lang="RUS">
+							<surname>Магомедов</surname>
+							<initials>Я. Б.</initials>
+							<orgName>Институт физики ДНЦ РАН</orgName>
+						</individInfo>
+					</author>
+					<author num="002">
+						<individInfo lang="RUS">
+							<surname>Гаджиев</surname>
+							<initials>Г. Г.</initials>
+							<email>Gadjiev@mail.ru</email>
+							<orgName>Институт физики ДНЦ РАН</orgName>
+						</individInfo>
+					</author>
+					<author num="003">
+						<individInfo lang="RUS">
+							<surname>Омаров</surname>
+							<initials>З. М.</initials>
+							<orgName>Институт физики ДНЦ РАН</orgName>
+						</individInfo>
+					</author>
+				</authors>
+				<artTitles>
+					<artTitle lang="RUS">ТЕМПЕРАТУРНАЯ ЗАВИСИМОСТЬ ТЕПЛОПРОВОДНОСТИ И ЭЛЕКТРОПРОВОДНОСТИ Bi2Te3 И ЕГО РАСПЛАВА</artTitle>
+				</artTitles>
+				<abstracts>
+					<abstract lang="RUS">Исследована температурная зависимость теплопроводности и электропроводности теллурида висмута и его расплава. Установлено, что основными механизмами теплопроводности как в твердом, так и в жидком состоянии являются фононный (молекулярный в расплаве), электронный и биполяр- ный. В области собственной проводимости и в расплаве значительный вклад в теплопроводность вно- сит биполярная теплопроводность. При плавлении Bi2Te3 полупроводниковые свойства сохраняются.</abstract>
+				</abstracts>
+				<text lang="RUS">
+					Как известно, основой зонной теории полупроводников является представление о стро- гойпериодичностикристаллическихрешеток.
+					Однако,исследованиеэлектропроводности,тер- моэдс и ряда других свойств некоторых полупроводников и их расплавов [2, 3] 
+					показало, что полупроводниковые свойства сохраняются и при структурном разупорядочении после плавле- ния.
+					Всвязисэтимисследованиекинетическихсвойствполупроводниковвобластиплавления и жидкого состояния имеет важное значение для 
+					теории для выяснения влияния структурного разупорядочения на зонную структуру и механизмы переноса заряда и тепла в 
+				</text>
+				<keywords>
+					<kwdGroup lang="ANY">
+						<keyword>Принципиальная схема установки</keyword>
+					</kwdGroup>
+				</keywords>
+				<references>
+					<reference>Иоффе А. Ф. Полупроводниковые термоэлементы. – М.: Из-во АН СССР, 1956. – 150 с.</reference>
+					<reference>IoffeA. F, RegelA. R. Progress in Semiconductors. – London: Pergamon Press, 1960. – V. 4. – P. 237.</reference>
+					<reference>Глазов В. М., Чижевская С. Н., Глаголева Н. Н. Жидкие полупроводники. – М.: Наука, 1967. – 240 с.</reference>
+					<reference>Глазов В. М., Крестовников А. Н., Глаголева Н. Н. // Изв. АН СССР. Серия неорганич. материалы. – 1966. – Т. 2. – Вып. 3. – С. 453.</reference>
+					<reference>Федоров В. И., Стильбанс Л. С. // ТВТ. – 1967. – Т. 5. – Вып. 2.   Температурная зависимость теплопровод- ности и электропроводности bi2te3 и его расплава</reference>
+					<reference>Кутасов В. А., Смирнов И. А. // Изв. АН СССР. Серия неорганич. материалы. – 1967. – Т. 3. – С. 380.</reference>
+					<reference>Гуриева Е. А., Кутасов В. А., Смирнов И. А. // ФТТ. – 1964. – Т. 6. – С. 3002.</reference>
+					<reference>Охотин А. С., Пушкарский А. С., Горбачев В. В. Теплофизические свойства полупровод- ников. – М.: Атомиздат, 1972. – 197 с.</reference>
+					<reference>Драбл Дж., Голдсмид Г. Теплопроводность полупроводников. – М.: Из-во ИЛ, 1963. – 260 с.</reference>
+					<reference>Магомедов Я. Б., Алиев С. Н., Айдамиров М. А., Лугуева Н. В. // ПТЭ. – 2003. – № 6. – С. 117.</reference>
+					<reference>Магомедов Я. Б. // ТВТ.– 1990. – Т. 28. – С. 3965.</reference>
+					<reference>Регель А. Р., Глазов В. М. Закономерности формирования структуры электронных рас- плавов. – М.: Наука, 1982. – 320 с.</reference>
+				</references>
+				<files>
+					<file>gadjiev121x1.jpg</file>
+					<file>gadjiev122x1.jpg</file>
+					<file>gadjiev123x1.jpg</file>
+					<file>gadjiev124x1.jpg</file>
+					<file>gadjiev125x1.jpg</file>
+				</files>
+				<dates>
+					<dateReceived>12.08.2013</dateReceived>
+				</dates>
+			</article>
+		</articles>
+	</issue>
+</journal>
--- scripts/script1.php
+++ scripts/script1.php
@@ -1,3 +1,5 @@
-<?php
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+print_r($this->account);exit;
 
 ?>
