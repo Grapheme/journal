b2f763681e0112f2ec4785858820279e9a559e2f
Владимир <vkharseev@gmail.com>
2014-12-29 19:36:31 +0300

    Список годов

--- application/config/database.php
+++ application/config/database.php
@@ -5,7 +5,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = "";
+$db['default']['password'] = "saijiedien";
 $db['default']['database'] = "ptint";
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = "";
